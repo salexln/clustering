9dd910c008d2be5e27c738d25624848492ab2c19
bdelpizzo <delpizzo.b@gmail.com>
2015-02-27 11:50:09 +0100

    Extraction of Fuzzy sets from FCM clustering model
    First draw

--- FuzzyCMeansModel.scala
+++ FuzzyCMeansModel.scala
@@ -20,14 +20,19 @@ package org.apache.spark.mllib.clustering
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.{ Vector, Vectors }
+import breeze.linalg.{ DenseVector => BDV, Vector => BV, norm => breezeNorm }
 
+class FuzzyCMeansModel(centroids: Array[BreezeVectorWithNorm],
+                       m: Double) extends Serializable {
 
-
-class FuzzyCMeansModel(centroids: Array[BreezeVectorWithNorm], 
-     m: Double) extends Serializable {
-
-  /** Total number of clusters. */
-  def c: Int = centroids.length
+  /** @param c: Total number of clusters. */
+  private def c: Int = centroids.length
+  /**
+   * @param treshold: data pre-filter.
+   *  Feed the reducer cutting out datums with small memebership degree
+   *  to the j-th  prototype
+   */
+  private def treshold = 0.1
 
   /** Print the clusters center. */
   def Centers() {
@@ -36,13 +41,102 @@ class FuzzyCMeansModel(centroids: Array[BreezeVectorWithNorm],
       println(centroids.apply(i).vector)
     }
   }
+
+  /**
+   * Retrieve the membership degree for each center
+   */
+  def getU(data: RDD[Vector]) = {
+    val breezeData = formatData(data)
+    //val indexedData = breezeData.zipWithIndex()
+    val sc = breezeData.sparkContext
+    val dimNumb = data.first().size
+    val broadcastCenters = sc.broadcast(centroids)
+    val broadcastCorrection = sc.broadcast(EPSILON)
+
+    val mapper = breezeData.mapPartitions { points =>
+      val pointsCopy = points.duplicate
+      val nPoints = pointsCopy._1.length
+      val singleDist = Array.fill[Double](c)(0)
+      val numDist = Array.fill[Double](c)(0)
+      val membershipMatrix = Array.ofDim[Double](nPoints, c)
+      val datums = Array.fill(nPoints)(BDV.zeros[Double](dimNumb).asInstanceOf[BV[Double]])
+      var i = 0
+
+      points.foreach { point =>
+        var denom = 0.0
+        for (j <- 0 until c) {
+          singleDist(j) = (KMeans.fastSquaredDistance(point, broadcastCenters.value(j)) +
+            broadcastCorrection.value)
+          numDist(j) = math.pow(singleDist(j), (1 / (m - 1)))
+          denom += (1 / numDist(j))
+        }
+        for (j <- 0 until c) {
+          val u = (numDist(j) * denom) //uij^m  
+          membershipMatrix(i)(j) = (1 / u)
+        }
+        datums(i) = point.vector
+        i += 1
+      }
+
+      /**
+       *  Split elements around cluster's prototype
+       *  var sign will be 0 if the datum's feature is greater (above) the cluster's prototype for
+       *  that feature
+       */
+      val outMapper = for (prototN <- 0 until c; elem <- 0 until nPoints; feature <- 0 until dimNumb) yield {
+        var sign = "p"     
+        if (datums(elem)(feature) >= broadcastCenters.value(prototN).vector(feature)) {sign = "p"}
+        else {sign = "n"}
+        ( (prototN, feature, sign), (membershipMatrix(elem)(prototN), datums(elem)(feature)) )
+      }
+      outMapper.iterator
+    }.filter(f => f._2._1 > treshold )
+    
+    
+      
+      
+      
+    
+    
+    
+    
+
+  }
   
+    
+  
+  
+  
+
+  /**
+   * Helper methods for lazy evaluation of a factor EPSILON
+   * to avoid division by zero while computing membership degree
+   * for each datum to each center
+   */
+  private lazy val EPSILON = {
+    var eps = 1.0
+    while ((1.0 + (eps / 2.0)) != 1.0) {
+      eps /= 2.0
+    }
+    eps
+  }
+
+  private def formatData(data: RDD[Vector]): RDD[BreezeVectorWithNorm] = {
+    // Compute squared norms and cache them.
+    val norms = data.map(v => breezeNorm(v.toBreeze, 2.0))
+    norms.persist()
+    val breezeData = data.map(_.toBreeze).zip(norms).map {
+      case (v, norm) =>
+        new BreezeVectorWithNorm(v, norm)
+    }
+    breezeData
+  }
+
   /**
    * Retrieve Centers' Vectors
-   */      
-   def getCenters(): Array[Vector] = {
-     centroids.map { c => Vectors.fromBreeze(c.vector) }
-   }
-  
-  
+   */
+  def getCenters(): Array[Vector] = {
+    centroids.map { c => Vectors.fromBreeze(c.vector) }
+  }
+
 }
